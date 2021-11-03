57048c6a907d10b9d6a6816eb60b3bdc25d11aaf
santoshmrp <smileforsantosh@gmail.com>
2021-11-03 13:45:58 +0530

    second commit

--- src/test/java/com/operations/VehicleServiceTest.java
+++ src/test/java/com/operations/VehicleServiceTest.java
@@ -1,12 +1,20 @@
 package com.operations;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
+import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.operations.request.UserRequest;
 import com.operations.request.VehicleRequest;
+import com.operations.response.UserResponse;
+import com.operations.response.VehicleResponse;
+import com.operations.service.UserService;
 import com.operations.service.VehicleService;
 
 @Component
@@ -15,8 +23,11 @@ public class VehicleServiceTest {
 	@Autowired
 	private VehicleService vehicleService;
 
+	@Autowired
+	private UserService userService;
+
 	@Test
-	public void addUserTest() {
+	public void addVehicleTest() {
 		VehicleRequest vehicleRequest = new VehicleRequest();
 		vehicleRequest.setId(3L);
 		vehicleRequest.setVehicleId("ghi");
@@ -24,5 +35,29 @@ public class VehicleServiceTest {
 		vehicleRequest.setVehicleNo("ghi");
 		String response = vehicleService.addVehicle(vehicleRequest);
 		assertEquals("Success", response);
+	}
+
+	@Test
+	public void getVehicleListTest() {
+		List<VehicleResponse> response = vehicleService.getVehicleList();
+		assertThat(response, IsEmptyCollection.empty());
+	}
+
+	@Test
+	public void addUserTest() {
+		UserRequest userRequest = new UserRequest();
+		userRequest.setId(3L);
+		userRequest.setEmail("smileforsantosh@gmail.com");
+		userRequest.setFirstName("santosh");
+		userRequest.setLastName("miriyala");
+		userRequest.setMobile("9966171071");
+		String response = userService.addUser(userRequest);
+		assertEquals("Success", response);
+	}
+
+	@Test
+	public void getUserListTest() {
+		List<UserResponse> response = userService.getUserList();
+		assertThat(response, IsEmptyCollection.empty());
 	}
 }
